361934562be87048eddb3dafa452a554ce8cbbcb
Haosen Li <69605423+haosenli@users.noreply.github.com>
2022-11-03 16:02:36 -0700

    Delete src directory

--- src/utils/formatting.py
+++
@@ -1,8 +0,0 @@
-import re
-
-
-def wordtrie_format(word: str):
-    """Returns a str word formatted for the WordTrie."""
-    # return re.sub(r'[^a-zA-Z0-9]', '', word.lower())
-    return re.sub('-|:|\s+', '', word.lower())
-
