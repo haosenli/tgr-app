1038b078c41962ff2c561d08ad883c4a02e8284a
StevenT07 <57376186+StevenT07@users.noreply.github.com>
2022-12-01 11:33:54 -0800

    Add the feature to take front and back with one button press

--- camera/App.js
+++ camera/App.js
@@ -11,6 +11,8 @@ export default function App() {
   const [hasMediaLibraryPermission, setHasMediaLibraryPermission] = useState();
   const [photo, setPhoto] = useState();
   const [type, setType] = useState(CameraType.back);
+  const [backPhoto, setBackPhoto] = useState();
+  const [backPhotoReady, setBackPhotoReady] = useState(false);
 
   useEffect(() => {
     (async () => {
@@ -33,13 +35,21 @@ export default function App() {
       base64: true,
       exif: false
     };
-
-    let newPhoto = await cameraRef.current.takePictureAsync(options);
-    console.log(newPhoto.base64);
-    setPhoto(newPhoto);
+    if (!photo) {
+      let newPhoto = await cameraRef.current.takePictureAsync(options);
+      toggleCameraType();
+      setPhoto(newPhoto);
+      //takePic();
+    }
+    else if (!backPhoto) {
+      let otherPhoto = await cameraRef.current.takePictureAsync(options);
+      setBackPhoto(otherPhoto);
+    }
+    
+    
   };
 
-  if (photo) {
+  if (photo && backPhoto) {
     let sharePic = () => {
       shareAsync(photo.uri).then(() => {
         setPhoto(undefined);
@@ -51,10 +61,11 @@ export default function App() {
         setPhoto(undefined);
       });
     };
-
+    
     return (
       <SafeAreaView style={styles.container}>
         <Image style={styles.preview} source={{ uri: "data:image/jpg;base64," + photo.base64 }} />
+        <Image style={styles.preview} source={{ uri: "data:image/jpg;base64," + backPhoto.base64 }} />
         <Button title="Share" onPress={sharePic} />
         {hasMediaLibraryPermission ? <Button title="Save" onPress={savePhoto} /> : undefined}
         <Button title="Discard" onPress={() => setPhoto(undefined)} />
@@ -64,8 +75,20 @@ export default function App() {
   function toggleCameraType() {
     setType(current => (current === CameraType.back ? CameraType.front : CameraType.back));
   }
+  let takeBackPhoto= async () => {
+    let options = {
+      quality: 1,
+      base64: true,
+      exif: false
+    };
+    if (!backPhoto && photo) {
+      let otherPhoto = await cameraRef.current.takePictureAsync(options);
+      setBackPhoto(otherPhoto);
+    }
+  }
+  
   return (
-    <Camera style={styles.container} ref={cameraRef} type={type}>
+    <Camera style={styles.container} ref={cameraRef} type={type} onCameraReady={takeBackPhoto} >
       <View style={styles.buttonContainer}>
         <Button title="Take Pic" onPress={takePic} />
         <Button title="Reverse" onPress={toggleCameraType}/>
