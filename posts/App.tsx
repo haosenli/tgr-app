b103f963214ce39e55c1dac05ad8067cb775c053
AntLe12 <le672869@gmail.com>
2022-12-28 14:08:33 -0800

    Blank setup (cleared startup components)

--- posts/App.tsx
+++ posts/App.tsx
@@ -3,15 +3,14 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.posts}>
+      
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  posts: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
