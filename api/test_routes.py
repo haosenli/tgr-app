a68b40f60ad8e9d6f6073abe7a35ee8e19dc2541
Haosen Li <haosenli@Haosens-MacBook-Pro-15.local>
2022-12-31 18:11:23 -0800

    updated flask app port for oracle cloud

--- api/test_routes.py
+++ api/test_routes.py
@@ -61,4 +61,4 @@ def test_get_posts():
 
 # for testing purposes
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
